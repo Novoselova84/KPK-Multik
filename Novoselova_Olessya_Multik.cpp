2da68daffa60df4b02256eee95a5a8ceb1a9d761
Novoselova84 <82278786+Novoselova84@users.noreply.github.com>
2021-04-20 22:56:53 +0500

    Добавление звукового оформления сцен
    Получилось прикольно)))

--- Novoselova_Olessya_Multik.cpp
+++ Novoselova_Olessya_Multik.cpp
@@ -6,7 +6,7 @@
 //+          Мультик создан в рамках КПК
 //+          по информатике "Наука в регионы".
 //
-//+  $date   2021 год.
+//+  $date             2021 год.
 //
 //+  $author Новоселова Олеся Анатольевна
 //+          учитель информатики МАОУ «МЛ №1»
@@ -73,6 +73,7 @@ void Author         ();
 int main            ()
     {
     txBegin ();
+
     txCreateWindow (1400, 800);
 
     Hello           ();
@@ -97,14 +98,16 @@ void Hello ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/Hello.wav");
+
     int t = 0;
-    while (t <= 120)
+    while (t <= 100)
         {
         DrawBascground ();
 
         txSetColor     (RGB (128, 0, 0), 50);
         txSelectFont   ("Gabriola", 300);
-        txDrawText     (20, 300 - t*2, 1210, 785 - t*2, "Привет, друзья!");
+        txDrawText     (20, 300 - t*1.5, 1210, 785 - t*1.5, "Привет, друзья!");
 
         DrawSun        (600, 340, 1.2, 1.2, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0),
                         TX_WHITE, RGB (128, 0, 255), (t/30)%2 * 1, (t/40)%2 * 1, 0, (t/30)%2 * 1, (t/20)%2 * 1);
@@ -113,12 +116,16 @@ void Hello ()
         t ++ ;
         }
 
+    txPlaySound (NULL);
+
     txEnd ();
     }
 
 void TitleMultika ()
     {
     txBegin ();
+
+    txPlaySound ("Sounds/TitleMultika.wav");
 
     int t = 0;
     while (t <= 60)
@@ -145,6 +152,8 @@ void TitleMultika ()
         t ++ ;
         }
 
+    txPlaySound (NULL);
+
     txEnd ();
     }
 
@@ -152,26 +161,30 @@ void Loneliness ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/Loneliness.wav");
+
     int t = 0;
     while (t <= 250)
         {
         DrawBascground ();
-        DrawOcean      ( 15,       320, 1  , 1  , RGB (77, 84, 225), 0);
-        DrawSmallCloud (500 + t/2, 100, 1  , 1  , TX_WHITE, 0);
-        DrawBigCloud   (100 + t/2,  50, 1  , 1  , TX_WHITE, 0, 0);
-        DrawSun        (120 + t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0), TX_WHITE,
+        DrawOcean      ( 15,       320, 1,   1,   RGB (77, 84, 225), 0);
+        DrawSmallCloud (500 + t/2, 100, 1,   1,   TX_WHITE, 0);
+        DrawBigCloud   (100 + t/2,  50, 1,   1,   TX_WHITE, 0, 0);
+        DrawSun        (150 + t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0), TX_WHITE,
                         RGB (128, 0, 255), (t/30)%2 * 1, 0, 0, 0, (t/40)%2 * 1);
         DrawWhale      (800 - t/2, 400, 0.7, 0.7, RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/40)%2 * 1);
         DrawWhale      (600 - t/2, 500, 0.8, 0.8, RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/40)%2 * 1, (t/40)%2 * 1, (t/40)%2 * 1);
-        DrawWhale      (800 - t/2, 600, 1  , 1  , RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/40)%2 * 1);
-        DrawIsland     (150,       650, 1  , 1  , RGB (  0, 128,   0), TX_YELLOW, (t/30)%2 * 1);
-        DrawMan        (350 + t/2, 550, 1  , 1  , RGB (192, 192, 192), TX_RED, TX_WHITE,
+        DrawWhale      (800 - t/2, 600, 1,   1,   RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/40)%2 * 1);
+        DrawIsland     (150,       650, 1,   1,   RGB (  0, 128,   0), TX_YELLOW, (t/30)%2 * 1);
+        DrawMan        (350 + t/2, 550, 1,   1,   RGB (192, 192, 192), TX_RED, TX_WHITE,
                         RGB (0, 128, 128), (t/20)%2 * 1, (t/30)%2 * 1, (t/30)%2 * 1, 0);
         DrawPalma      (200,       720, 1.2, 1.2, RGB (108,  54,   0), RGB (0, 128, 0), TX_BROWN, (t/30)%2 * 1, 0);
 
         txSleep (SleepTime);
         t ++ ;
         }
+
+    txPlaySound (NULL);
 
     txEnd ();
     }
@@ -180,27 +193,31 @@ void Hope ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/volnyi.wav");
+
     int t = 0;
     while (t <= 200)
         {
         DrawBascground ();
-        DrawOcean      (  15,       320, 1,   1  , RGB (77, 84, 225), 0);
-        DrawSmallCloud ( 500 + t/2, 100, 1,   1  , TX_WHITE, 0);
-        DrawBigCloud   ( 100 + t/2,  50, 1,   1  , TX_WHITE, 0, 0);
-        DrawSun        (1100 - t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0),
+        DrawOcean      (  15,       320, 1,   1,   RGB (77, 84, 225), 0);
+        DrawSmallCloud ( 500 + t/2, 100, 1,   1,   TX_WHITE, 0);
+        DrawBigCloud   ( 100 + t/2,  50, 1,   1,   TX_WHITE, 0, 0);
+        DrawSun        (1050 - t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0),
                         TX_WHITE, RGB (128, 0, 255), 0, (t/40)%2 * 1, 0, 0, (t/40)%2 * 1);
         DrawShip       ( 250 + t/2, 300, 0.3, 0.2, TX_GREY, TX_YELLOW, RGB (164, 82, 0), TX_PINK, TX_CYAN, TX_GREY, 0, 0, 0);
         DrawWhale      ( 800 - t/2, 500, 0.9, 0.9, RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/30)%2 * 1);
-        DrawIsland     ( 150,       650, 1  , 1  , RGB (  0, 128,   0), TX_YELLOW, (t/30)%2 * 1);
+        DrawIsland     ( 150,       650, 1,   1,   RGB (  0, 128,   0), TX_YELLOW, (t/30)%2 * 1);
         DrawMan        ( 600,       550 - (t/10)%2 * 10, 1, 1, RGB (192, 192, 192), TX_RED, TX_WHITE,
                         RGB (0, 128, 128), (t/20)%2 * 1, 0, (t/30)%2 * 1, 0);
-        DrawTorch      ( 500,       520 - (t/10)%2 * 10, 1  , 1  , RGB (108,  54, 0), RGB (255, 0, 0), (t/20)%2 * 1);
-        DrawTreasures  ( 900,       650,                 1  , 1  , RGB (128, 128, 0), TX_YELLOW, TX_RED, 0, 0);
+        DrawTorch      ( 500,       520 - (t/10)%2 * 10, 1,   1,   RGB (108,  54, 0), RGB (255, 0, 0), (t/20)%2 * 1);
+        DrawTreasures  ( 900,       650,                 1,   1,   RGB (128, 128, 0), TX_YELLOW, TX_RED, 0, 0);
         DrawPalma      ( 200,       720,                 1.2, 1.2, RGB (108,  54, 0), RGB (0, 128, 0), TX_BROWN, 0, (t/40)%2 * 1);
 
         txSleep (SleepTime);
         t ++ ;
         }
+
+    txPlaySound (NULL);
 
     txEnd ();
     }
@@ -209,25 +226,29 @@ void Fear ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/udar-molnii.wav");
+
     int t = 0;
     while (t <= 400)
         {
         DrawBascground ();
-        DrawOcean      (  15,       320, 1  , 1  , RGB (77, 84, 225), 1);
-        DrawSmallCloud ( 500 + t/2, 100, 1  , 1  , TX_LIGHTGRAY, (t/30)%2 * 1);
-        DrawBigCloud   ( 100 + t/2,  50, 1  , 1  , TX_LIGHTGRAY, (t/30)%2 * 1, (t/30)%2 * 1);
-        DrawSun        (1100 - t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238),
+        DrawOcean      (  15,       320, 1,   1, RGB (77, 84, 225), 1);
+        DrawSmallCloud ( 500 + t/2, 100, 1,   1, TX_LIGHTGRAY, (t/30)%2 * 1);
+        DrawBigCloud   ( 100 + t/2,  50, 1,   1, TX_LIGHTGRAY, (t/30)%2 * 1, (t/30)%2 * 1);
+        DrawSun        (1050 - t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238),
                         RGB (66, 132, 0), TX_WHITE, RGB (128, 0, 255), 0, (t/30)%2 * 1, (t/30)%2 * 1, 0, 0);
         DrawShip       ( 400 + t/2, 300, 0.7, 0.6, TX_GREY, RGB (255, 236, 236), RGB (164, 82, 0),
                         RGB (63, 63, 63), RGB (63, 63, 63), TX_GREY, 0, 0, 1);
-        DrawIsland     ( 150,       650,                 1  , 1  , RGB (  0, 128,   0), TX_YELLOW, 1);
-        DrawMan        ( 400 + t/4, 550 - (t/10)%2 * 10, 1  , 1  , RGB (192, 192, 192), TX_RED, TX_WHITE, RGB (0, 128, 128), 1, 0, 0, (t/40)%2 * 1);
-        DrawTreasures  ( 900 - t/6, 650 - (t/20)%2 *  5, 1  , 1  , RGB (128, 128,   0), TX_YELLOW, TX_RED, 1, (t/50)%2 * 1);
+        DrawIsland     ( 150,       650,                 1,   1,   RGB (  0, 128,   0), TX_YELLOW, 1);
+        DrawMan        ( 400 + t/4, 550 - (t/10)%2 * 10, 1,   1,   RGB (192, 192, 192), TX_RED, TX_WHITE, RGB (0, 128, 128), 1, 0, 0, (t/40)%2 * 1);
+        DrawTreasures  ( 900 - t/6, 650 - (t/20)%2 *  5, 1,   1,   RGB (128, 128,   0), TX_YELLOW, TX_RED, 1, (t/50)%2 * 1);
         DrawPalma      ( 200,       720,                 1.2, 1.2, RGB (108,  54,   0), RGB (0, 128, 0), TX_BROWN, (t/40)%2 * 1, 0);
 
         txSleep (SleepTime);
         t ++ ;
         }
+
+    txPlaySound (NULL);
 
     txEnd ();
     }
@@ -236,26 +257,30 @@ void Salvation ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/Salvation.wav");
+
     int t = 0;
     while (t <= 300)
         {
         DrawBascground ();
-        DrawOcean      ( 15,       320, 1  , 1  , RGB (77, 84, 225), 0);
-        DrawSmallCloud (500 + t/2, 100, 1  , 1  , TX_WHITE, 0);
-        DrawBigCloud   (100 + t/2,  50, 1  , 1  , TX_WHITE, 0, 0);
-        DrawSun        (120 + t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0),
+        DrawOcean      ( 15,       320, 1,   1,   RGB (77, 84, 225), 0);
+        DrawSmallCloud (500 + t/2, 100, 1,   1,   TX_WHITE, 0);
+        DrawBigCloud   (100 + t/2,  50, 1,   1,   TX_WHITE, 0, 0);
+        DrawSun        (150 + t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0),
                         TX_WHITE, RGB (128, 0, 255), (t/30)%2 * 1, (t/30)%2 * 1, 0, 0, (t/40)%2 * 1);
         DrawShip       (450 + t/2, 300 + t*0.2, 0.6, 0.6, TX_GREY, TX_YELLOW, RGB (164, 82, 0), TX_PINK, TX_CYAN,
                         TX_GREY, 0, (t/40)%2 * 1, 0);
         DrawIsland     (150,       650,                 1, 1, RGB (  0, 128,   0), TX_YELLOW, (t/30)%2 * 1);
         DrawMan        (600,       550 - (t/10)%2 * 10, 1, 1, RGB (192, 192, 192), TX_RED,
                         TX_WHITE, RGB (0, 128, 128), (t/40)%2 * 1, (t/40)%2 * 1, (t/40)%2 * 1, 0);
-        DrawTreasures  (900,       650, 1  , 1  , RGB (128, 128, 0), TX_YELLOW, TX_RED, 0, 0);
+        DrawTreasures  (900,       650, 1,   1,   RGB (128, 128, 0), TX_YELLOW, TX_RED, 0, 0);
         DrawPalma      (200,       720, 1.2, 1.2, RGB (108,  54, 0), RGB (0, 128, 0), TX_BROWN, 0, (t/40)%2 * 1);
 
         txSleep (SleepTime);
         t ++ ;
         }
+
+    txPlaySound (NULL);
 
     txEnd ();
     }
@@ -264,26 +289,30 @@ void Happiness ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/Happiness.wav");
+
     int t = 0;
     while (t <= 200)
         {
         DrawBascground ();
-        DrawOcean      ( 15,       320, 1,   1  , RGB (77, 84, 225), 0);
-        DrawSmallCloud (500 + t/2, 100, 1,   1  , TX_WHITE, 0);
-        DrawBigCloud   (100 + t/2,  50, 1,   1  , TX_WHITE, 0, 0);
+        DrawOcean      ( 15,       320, 1,   1,   RGB (77, 84, 225), 0);
+        DrawSmallCloud (500 + t/2, 100, 1,   1,   TX_WHITE, 0);
+        DrawBigCloud   (100 + t/2,  50, 1,   1,   TX_WHITE, 0, 0);
         DrawSun        (600 + t/6, 130, 0.5, 0.5, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0),
                         TX_WHITE, RGB (128, 0, 255), (t/30)%2 * 1, (t/30)%2 * 1, 0, 0, (t/40)%2 * 1);
         DrawWhale      (500 - t/2, 360, 0.5, 0.6, RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/30)%2 * 1);
         DrawShip       (700,       450, 0.8, 0.8, TX_GREY, TX_YELLOW, RGB (164, 82, 0), TX_PINK, TX_CYAN, TX_GREY, 1, 0, 0);
-        DrawIsland     (150,       650, 1  , 1  , RGB (  0, 128,   0), TX_YELLOW, 1);
+        DrawIsland     (150,       650, 1,   1,   RGB (  0, 128,   0), TX_YELLOW, 1);
         DrawMan        (620,       310,                 0.8, 0.8, RGB (192, 192, 192), TX_RED, TX_WHITE, RGB (0, 128, 128), 0, (t/30)%2 * 1, 1, 0);
         DrawTreasures  (750,       385 - (t/10)%2 * 10, 0.8, 0.8, RGB (128, 128,   0), TX_YELLOW, TX_RED, 0, 0);
-        DrawIsland     (150,       650,                 1  , 1  , RGB (  0, 128,   0), TX_YELLOW, 1);
+        DrawIsland     (150,       650,                 1,   1,   RGB (  0, 128,   0), TX_YELLOW, 1);
         DrawPalma      (200,       720,                 1.2, 1.2, RGB (108,  54,   0), RGB (0, 128, 0), TX_BROWN, 0, (t/40)%2 * 1);
 
         txSleep (SleepTime);
         t ++ ;
         }
+
+    txPlaySound (NULL);
 
     txEnd ();
     }
@@ -292,27 +321,31 @@ void Enjoyment ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/Enjoyment.wav");
+
     int t = 0;
-    while (t <= 200)
+    while (t <= 180)
         {
         DrawBascground ();
-        DrawOcean      ( 15,       320,  1  ,   1  , RGB (77, 84, 225), 0);
-        DrawSmallCloud (500 + t/2, 100,  1  ,   1  , TX_WHITE, 0);
-        DrawBigCloud   (100 + t/2,  50,  1  ,   1  , TX_WHITE, 0, 0);
-        DrawSmallCloud (700 + t/2,  50,  1  ,   1  , TX_WHITE, 0);
-        DrawBigCloud   (300 + t/2, 100,  1  ,   1  , TX_WHITE, 0, 0);
+        DrawOcean      ( 15,       320,  1,     1, RGB (77, 84, 225), 0);
+        DrawSmallCloud (500 + t/2, 100,  1,     1, TX_WHITE, 0);
+        DrawBigCloud   (100 + t/2,  50,  1,     1, TX_WHITE, 0, 0);
+        DrawSmallCloud (700 + t/2,  50,  1,     1, TX_WHITE, 0);
+        DrawBigCloud   (300 + t/2, 100,  1,     1, TX_WHITE, 0, 0);
         DrawSun        (600 + t/6, 130,  0.5,   0.5, RGB (255, 255, 0), RGB (0, 238, 238), RGB (66, 132, 0),
                         TX_WHITE, RGB (128, 0, 255), (t/30)%2 * 1, 0, 0, 1, (t/40)%2 * 1);
         DrawWhale      (800 - t/2, 500, 0.7, 0.8, RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/30)%2 * 1);
         DrawWhale      (600 - t/2, 430, 0.6, 0.8, RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/30)%2 * 1);
-        DrawWhale      (700 - t/2, 600, 0.9, 1  , RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/30)%2 * 1);
+        DrawWhale      (700 - t/2, 600, 0.9, 1,   RGB (128, 128, 128), TX_BLUE, RGB (0, 128, 255), (t/30)%2 * 1, (t/30)%2 * 1, (t/30)%2 * 1);
         DrawShip       (750 + t/2, 300, 0.3, 0.3, TX_GREY, TX_YELLOW, RGB (164, 82, 0), TX_PINK, TX_CYAN, TX_GREY, 0, 0, 0);
-        DrawIsland     (150,       650, 1  , 1  , RGB (  0, 128, 0), TX_YELLOW, 1);
+        DrawIsland     (150,       650, 1,   1,   RGB (  0, 128, 0), TX_YELLOW, 1);
         DrawPalma      (200,       720, 1.2, 1.2, RGB (108,  54, 0), RGB (0, 128, 0), TX_BROWN, 0, (t/30)%2 * 1);
 
         txSleep (SleepTime);
         t ++ ;
         }
+
+    txPlaySound (NULL);
 
     txEnd ();
     }
@@ -321,8 +354,10 @@ void TobeContinued ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/TobeContinued.wav");
+
     int t = 0;
-    while (t <= 70)
+    while (t <= 100)
         {
         DrawBascground ();
 
@@ -334,6 +369,8 @@ void TobeContinued ()
         t ++ ;
         }
 
+    txPlaySound (NULL);
+
     txEnd ();
     }
 
@@ -341,8 +378,10 @@ void Author ()
     {
     txBegin ();
 
+    txPlaySound ("Sounds/TobeContinued.wav");
+
     int t = 0;
-    while (t <= 70)
+    while (t <= 90)
         {
         DrawBascground ();
 
@@ -357,6 +396,8 @@ void Author ()
         txSleep (SleepTime);
         t ++ ;
         }
+
+    txPlaySound (NULL);
 
     txEnd ();
     }
@@ -420,10 +461,10 @@ void DrawTV ()
 
 void DrawOcean (int x, int y, double sizeX, double sizeY, COLORREF oceanColor, double wave)
     {
-    if (wave >=1)
+    if (wave >= 1)
         {
-        txSetColor     (RGB (0, 0, 149));
-        txSetFillColor (RGB (0, 0, 149));
+        txSetColor     (RGB (80, 55, 219));
+        txSetFillColor (RGB (80, 55, 219));
         txRectangle    (x, y, x + 1200*sizeX, y + 470*sizeY);
         }
     else
@@ -725,10 +766,10 @@ void DrawPalma (int x, int y, double sizeX, double sizeY, COLORREF trunkColor, C
     txSetColor     (TX_GRAY, 2);
     txSetFillColor (coconutsColor);
     txCircle (x -  10*sizeX, y -  225                      *sizeY, (15 + magnificationCoconuts*10)*sizeX);
-    txCircle (x -  30*sizeX, y -  210                      *sizeY, (15 + magnificationCoconuts*10)*sizeX);
+    txCircle (x -  30*sizeX, y - (210 - fallingCoconut*120)*sizeY, (15 + magnificationCoconuts*10)*sizeX);
     txCircle (x,             y -  230                      *sizeY, (15 + magnificationCoconuts*10)*sizeX);
-    txCircle (x -  10*sizeX, y -  215                      *sizeY, (15 + magnificationCoconuts*10)*sizeX);
-    txCircle (x +  10*sizeX, y - (205 - fallingCoconut*100)*sizeY, (15 + magnificationCoconuts*10)*sizeX);
+    txCircle (x -  10*sizeX, y - (215 - fallingCoconut*150)*sizeY, (15 + magnificationCoconuts*10)*sizeX);
+    txCircle (x +  10*sizeX, y - (205 - fallingCoconut*170)*sizeY, (15 + magnificationCoconuts*10)*sizeX);
     }
 
 void DrawTreasures (int x, int y, double sizeX, double sizeY, COLORREF chestBoxColor, COLORREF chestLidColor,
